878c6c6df92ab1c1714ea96996b3c2dc69ca576a
Marharyta <margooowa@ukr.net>
2016-11-02 18:44:57 +0200

    directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,8 +7,12 @@ import {Recipe} from "../recipe";
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
-  recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs1.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg');
+  recipes: Recipe[] = [
+    new Recipe('Dummy1', 'Dummy1', 'http://www.leanphysiquemeals.co.uk/wp-content/uploads/2015/02/food1.jpg',[]),
+    new Recipe('Dummy1', 'Dummy2', 'http://www.leanphysiquemeals.co.uk/wp-content/uploads/2015/02/153902892-1024x683.jpg',[])
+  ];
+
+  // recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs1.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg');
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
